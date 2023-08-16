c341fcfe8dd32b47a96067a1d45aa14eb83acce6
Dairalir <daimanvm@gmail.com>
2023-08-16 18:25:00 +0200

    Utilisation lib jour ferié

--- src/Pages/Calendrier/Calendrier.jsx
+++ src/Pages/Calendrier/Calendrier.jsx
@@ -1,5 +1,6 @@
 import { Gantt } from 'gantt-task-react';
 import "gantt-task-react/dist/index.css";
+import lib from '../../lib'
 
 export function Calendrier(props) {
     if(props.modules.length === 0){
@@ -9,12 +10,14 @@ export function Calendrier(props) {
     return (
         <Gantt tasks={props.modules.map( (module) => {
 
+        console.log(lib.consume(module.debut, module.duree))
+
             const dateFin = new Date(module.debut)
             dateFin.setDate(dateFin.getDate() + module.duree);
 
             return {
                 start: new Date(module.debut),
-                end: dateFin,
+                end: new Date(lib.consume(module.debut, module.duree).fin) ,
                 name: module.nom,
                 id: module.id,
                 type:'task',
